e9a8a4847602d0c759a7d7775929644525e3d1be
Alex <a.mccanna@hotmail.com>
2023-12-18 21:38:56 -0800

    Add project files.

---
+++ UnemploymentAssistant/Models/JobApplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnemploymentAssistant.Models
+{
+    public class JobApplication
+    {
+        public JobApplication()
+        {
+            CompanyName = "default";
+            PositionName = "default";
+            AppliedDate = DateTime.Now;
+            Url = "default";
+        }
+
+        public JobApplication(string companyName, string positionName, DateTime appliedDate, string url)
+        {
+            CompanyName = companyName;
+            PositionName = positionName;
+            AppliedDate = appliedDate;
+            Url = url;
+        }
+
+        public string CompanyName { get; set; }
+
+        public string PositionName { get; set; }
+
+        public DateTime AppliedDate { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ UnemploymentAssistant/Models/JobApplicationManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnemploymentAssistant.Models
+{
+    public class JobApplicationManager
+    {
+        public static ObservableCollection<JobApplication> JobApplications = new ObservableCollection<JobApplication>() {
+            new JobApplication("Warner Bros Discovery", "Staff Software Engineer (R000074972)", DateTime.Today, "https://careers.wbd.com/global/en/job/R000074972/Staff-Software-Engineer"),
+        };
+
+        public static ObservableCollection<JobApplication> GetJobApplications()
+        {
+            return JobApplications;
+        }
+
+        public static void AddJobApplication(JobApplication job)
+        {
+            JobApplications.Add(job);
+        }
+    }
+}
